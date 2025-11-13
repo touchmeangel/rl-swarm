46ec4b3095bdf50d2f64a6953f78c85e78300527
touchmeangel <vodkavoid@proton.me>
2025-11-13 11:12:44 +0200

    -model name

--- run_rl_swarm.sh
+++ run_rl_swarm.sh
@@ -258,17 +258,17 @@ case $yn in
 esac
 
 
-echo -en $GREEN_TEXT
-read -p ">> Enter the name of the model you want to use in huggingface repo/name format, or press [Enter] to use the default model. " MODEL_NAME
-echo -en $RESET_TEXT
+# echo -en $GREEN_TEXT
+# read -p ">> Enter the name of the model you want to use in huggingface repo/name format, or press [Enter] to use the default model. " MODEL_NAME
+# echo -en $RESET_TEXT
 
-# Only export MODEL_NAME if user provided a non-empty value
-if [ -n "$MODEL_NAME" ]; then
-    export MODEL_NAME
-    echo_green ">> Using model: $MODEL_NAME"
-else
-    echo_green ">> Using default model from config"
-fi
+# # Only export MODEL_NAME if user provided a non-empty value
+# if [ -n "$MODEL_NAME" ]; then
+#     export MODEL_NAME
+#     echo_green ">> Using model: $MODEL_NAME"
+# else
+#     echo_green ">> Using default model from config"
+# fi
 #logout to prevent weird env issues, if it fails unset and try again
 if ! hf auth logout > /dev/null 2>&1; then
     unset HF_TOKEN
